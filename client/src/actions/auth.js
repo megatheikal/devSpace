d5db516e9cb9465b645e5ff5125cd875b9290bcc
m00m <mgaat.hkl@gmail.com>
2020-06-02 00:26:45 +0800

    Add education & experience

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,7 +6,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_PROFILLE
+  CLEAR_PROFILE
 } from "./types";
 import { setAlert } from "./alert";
 import axios from "axios";
@@ -93,6 +93,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout / Clear profile
 export const logout = () => dispatch => {
-  dispatch({ type: CLEAR_PROFILLE });
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
